54fa9927a5a893a39935d21a4bdfb8cbba064f9e
Nolan Wittmann <wittmann.nolan@gmail.com>
2022-03-14 20:14:49 +0100

    Projet fini

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus doloremque quae atque numquam fugit, ipsum aspernatur animi. Ratione esse, nihil alias repellat eos ullam voluptas necessitatibus fuga molestias accusamus culpa eligendi aliquam id eaque amet quo ipsum error. Cupiditate nemo eum quasi eius harum quia aspernatur asperiores amet, dolore laudantium!</p>
+        </div>
+    );
+};
+
+export default About;
